16135c349113de4735bb3aba17309481f9dd382f
Lamia EL RALIMI <elralimi.lamia@gmail.com>
2021-04-05 17:59:41 +0200

    🚧 Functional registration test with Selenium WIP

---
+++ tests/users/functional_tests/test_authentication.py
@@ -0,0 +1,58 @@
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+
+from selenium import webdriver
+
+# from selenium.webdriver.firefox.webdriver import WebDriver
+
+from selenium.webdriver.firefox.options import Options
+import os
+
+
+class SeleniumRegisterTest(StaticLiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        
+        firefox_options = Options()
+        firefox_options.add_argument("--headless")
+        geckodriver = os.getcwd() + "/geckodriver"
+        #cls.selenium = webdriver.Firefox(executable_path=geckodriver, options=firefox_options)
+        cls.selenium = webdriver.Firefox(executable_path=geckodriver)
+        cls.selenium.implicitly_wait(10)
+
+        # FONCTIONNE
+        #geckodriver = os.getcwd() + "/geckodriver"
+        #driver = webdriver.Firefox(executable_path=geckodriver)
+        #print("Firefox Browser Invoked")
+        #driver.get('http://google.com/')
+        #driver.quit()
+        # FIN DE FONCTIONNE
+        
+        # .selenium = WebDriver(geckodriver)
+        # cls.driver = webdriver.Chrome(
+        # chrome_options=chrome_options,
+        # executable_path=chrome_driver,
+        # )
+        # cls.selenium = webdriver.Chrome()
+        # cls.selenium.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+    
+    def test_register(self):
+        # Access register page and fill fields
+        self.selenium.get("%s%s" % (self.live_server_url, "/register/"))
+        email_input = self.selenium.find_element_by_name("email")
+        email_input.send_keys("usertest1@email.com")
+        username_input = self.selenium.find_element_by_name("username")
+        username_input.send_keys("usertest1")
+        password_input = self.selenium.find_element_by_name("password1")
+        password_input.send_keys("Password+1234")
+        confirm_password_input = self.selenium.find_element_by_name("password2")
+        confirm_password_input.send_keys("Password+1234")
+        # Click on button which registers + login automatically
+        self.selenium.find_element_by_class_name("btn").click()
+        # Checks if icon "mon_compte" in DOM, means logged in
+        self.selenium.find_element_by_id("mon_compte")
