df4091925b0f58dac4494c7d7fd1f552f430417c
Lamia EL RALIMI <lamia.elralimi@nova-technology.fr>
2021-04-05 20:24:06 +0200

    ♻️ Refactored functional test

--- tests/users/functional_tests/test_authentication.py
+++ tests/users/functional_tests/test_authentication.py
@@ -1,9 +1,6 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 
-# from selenium.webdriver.firefox.webdriver import WebDriver
-
-# from selenium.webdriver.firefox.options import Options
 import os
 
 
@@ -11,22 +8,9 @@ class SeleniumRegisterTest(StaticLiveServerTestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        
-        #firefox_options = Options()
-        #firefox_options.add_argument("--headless")
         geckodriver = os.getcwd() + "/geckodriver"
-        #cls.selenium = webdriver.Firefox(executable_path=geckodriver, options=firefox_options)
         cls.selenium = webdriver.Firefox(executable_path=geckodriver)
         cls.selenium.implicitly_wait(10)
-
-        # FONCTIONNE
-        #geckodriver = os.getcwd() + "/geckodriver"
-        #driver = webdriver.Firefox(executable_path=geckodriver)
-        #print("Firefox Browser Invoked")
-        #driver.get('http://google.com/')
-        #driver.quit()
-        # FIN DE FONCTIONNE
-
 
     @classmethod
     def tearDownClass(cls):
@@ -42,17 +26,11 @@ class SeleniumRegisterTest(StaticLiveServerTestCase):
         username_input.send_keys("usertest1")
         password_input = self.selenium.find_element_by_name("password1")
         password_input.send_keys("Password+1234")
-        confirm_password_input = self.selenium.find_element_by_name("password2")
+        confirm_password_input = self.selenium.find_element_by_name(
+            "password2"
+        )
         confirm_password_input.send_keys("Password+1234")
         # Click on button which registers + login automatically
         self.selenium.find_element_by_class_name("btn").click()
         # Checks if icon "mon_compte" in DOM, means logged in
         self.selenium.find_element_by_id("mon_compte")
-
-    """def test_login(self):
-        self.selenium.get("%s%s" % (self.live_server_url, "/login/"))
-        username_input = self.selenium.find_element_by_name("username")
-        username_input.send_keys("user1@gmail.com")
-        password_input = self.selenium.find_element_by_name("password")
-        password_input.send_keys("password1234")
-        self.selenium.find_element_by_class_name("btn").click()"""
